3f2aff86426219a6fe3487b325a4037118d6487f
dpthenicest <fortuneprecious17@gmail.com>
2025-10-16 23:18:35 +0100

    updated!

--- MethodsThatReturnValues/Program.cs
+++ MethodsThatReturnValues/Program.cs
@@ -128,3 +128,142 @@ bool IsPalindrome(string word)
   }
   return true;
 }
+
+Console.WriteLine("=======================================");
+Console.WriteLine("Exercise - Return arrays from methods");
+
+int oldTarget = 60;
+int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+int[] result = TwoCoins(coins, oldTarget);
+
+if (result.Length == 0)
+{
+  Console.WriteLine("No two coins make change");
+}
+else
+{
+  Console.WriteLine($"Change found at positions {result[0]} and {result[1]}");
+}
+
+int[] TwoCoins(int[] coins, int target)
+{
+  for (int curr = 0; curr < coins.Length; curr++)
+  {
+    for (int next = curr + 1; next < coins.Length; next++)
+    {
+      if (coins[curr] + coins[next] == target)
+      {
+        return new int[] { curr, next };
+      }
+    }
+  }
+
+  return new int[0];
+}
+
+Console.WriteLine("==============================================");
+Console.WriteLine("Find multiple pairs of coins that make change");
+
+int newTarget = 30;
+int[] newCoins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+int[,] newResult = MultipleTwoCoins(newCoins, newTarget);
+
+if (newResult.Length == 0)
+{
+  Console.WriteLine("No two coins make change");
+}
+else
+{
+  Console.WriteLine("Change found at positions:");
+  for (int i = 0; i < newResult.GetLength(0); i++)
+  {
+    if (newResult[i, 0] == -1)
+    {
+      break;
+    }
+    Console.WriteLine($"{newResult[i, 0]} and {newResult[i, 1]}");
+  }
+}
+
+int[,] MultipleTwoCoins(int[] coins, int target)
+{
+  int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+  int count = 0;
+
+  for (int curr = 0; curr < coins.Length; curr++)
+  {
+    for (int next = curr + 1; next < coins.Length; next++)
+    {
+      if (coins[curr] + coins[next] == target)
+      {
+        result[count, 0] = curr;
+        result[count, 1] = next;
+        count++;
+      }
+      if (count == result.GetLength(0))
+      {
+        return result;
+      }
+    }
+  }
+  return (count == 0) ? new int[0, 0] : result;
+}
+
+Console.WriteLine("==============================================");
+Console.WriteLine("Exercise - Complete the challenge to add methods to make the game playable");
+
+Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+
+if (ShouldPlay())
+{
+  PlayGame();
+}
+
+void PlayGame()
+{
+  var play = true;
+
+  while (play)
+  {
+    var target = random.Next(1, 6);
+    var roll = random.Next(1, 6);
+
+    Console.WriteLine($"Roll a number greater than {target} to win!");
+    Console.WriteLine($"You rolled a {roll}");
+    Console.WriteLine(WinOrLose(roll, target));
+    Console.WriteLine("\nPlay again? (Y/N)");
+
+    play = ShouldPlay();
+  }
+}
+
+bool ShouldPlay()
+{
+  string? answer = null;
+
+  answer = Console.ReadLine();
+  if (answer?.ToLower() == "y")
+  {
+    return true;
+  }
+
+  return false;
+}
+
+string WinOrLose(int roll, int target)
+{
+  if (roll > target)
+  {
+    return "You win!";
+  }
+  else if (roll == target)
+  {
+    return "It's a tie!";
+  }
+  else
+  {
+    return "You lose!";
+  }
+}
