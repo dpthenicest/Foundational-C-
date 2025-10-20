5d1ef1d5f427aed2a1dfdcdb45c54be9018a216f
dpthenicest <fortuneprecious17@gmail.com>
2025-10-20 15:23:56 +0100

    completed module 5

---
+++ PrinciplesOfCodeDebuggingAndExceptionHandling/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] students = new string[] { "Alice", "Bob", "Charlie", "Diana" };
+
+int studentCount = students.Length;
+
+Console.WriteLine($"The Final name is: {students[studentCount]}");
+
+
+ 
